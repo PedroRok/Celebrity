5c97bf3a9d732f668724964d2a5efa1937012677
rok <rexblanegamer@gmail.com>
2023-11-17 16:58:27 -0300

    CelebrityCommand

---
+++ src/main/java/com/githug/rok/celebrity/CelebrityCommand.java
@@ -0,0 +1,25 @@
+package com.githug.rok.celebrity;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * @author Rok, Pedro Lucas nmm. Created on 17/11/2023
+ * @project Celebrity
+ */
+public class CelebrityCommand implements TabExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
+    }
+}
